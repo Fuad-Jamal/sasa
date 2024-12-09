a0ebbb78b3ced11acbadd1a863fadb056fe7e28c
Fuad Jamal <jamalfuad34@gmail.com>
2024-12-09 14:14:43 +0200

    button

---
+++ button.js
@@ -0,0 +1,5 @@
+function js_style(){
+document.getElementById('text').style.fontSize= '70px'
+document.getElementById('text').style.fontFamily='Times New Roman'
+document.getElementById('text').style.color='green'
+}
